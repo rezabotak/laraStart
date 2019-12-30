171fa2ebf717778b9f41411975b3541341d3f4d9
rezabotak <rezaarakhman@gmail.com>
2019-12-30 23:55:55 +0700

    installing laravel passport and configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -50,6 +50,7 @@ Vue.use(VueRouter)
 const routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/developer', component: require('./components/Developer.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default }
 ]
 
@@ -88,6 +89,22 @@ window.Fire = new Vue()
 
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+// Passport
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
