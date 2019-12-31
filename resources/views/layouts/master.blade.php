9d7742d7bbaa42c5e6568e08104fb63f9f873307
rezabotak <rezaarakhman@gmail.com>
2019-12-31 17:19:15 +0700

    display profile picture in sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,7 +58,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
+                        @if (Auth::user()->photo === "profile.png")
                         <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
+                        @else
+                        <img src="./img/profile/{{ Auth::user()->photo }}" class="img-circle elevation-2"
+                            alt="User Image">
+                        @endif
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
