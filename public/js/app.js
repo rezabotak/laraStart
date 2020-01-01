6cdfad99a7e9b8b416235a84b858ca3328066b5f
rezabotak <rezaarakhman@gmail.com>
2020-01-01 15:15:00 +0700

    page not found

--- public/js/app.js
+++ public/js/app.js
@@ -79385,6 +79385,9 @@ var routes = [{
 }, {
   path: '/profile',
   component: __webpack_require__(/*! ./components/Profile.vue */ "./resources/js/components/Profile.vue")["default"]
+}, {
+  path: '*',
+  component: __webpack_require__(/*! ./components/NotFound.vue */ "./resources/js/components/NotFound.vue")["default"]
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
   mode: 'history',
